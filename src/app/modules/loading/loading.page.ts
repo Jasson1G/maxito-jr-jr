3417c0b97b231259d02ce73fcfdd9f65691db625
Jasson1G <103662186+Jasson1G@users.noreply.github.com>
2023-11-15 23:15:14 -0300

    Update loading.page.ts
    cambio pagina de carga

--- src/app/modules/loading/loading.page.ts
+++ src/app/modules/loading/loading.page.ts
@@ -14,6 +14,6 @@ export class LoadingPage implements OnInit {
     setTimeout(() => {
       // Redirigir a la página "login"
       this.router.navigateByUrl('/login');
-    }, 10000); // 10000 milisegundos = 10 segundos
+    }, 10000); // 6000 milisegundos = 10 segundos
   }
 }
